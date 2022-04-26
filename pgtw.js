8112d2cc785fb07fbad759434594e22f788041f3
Josef Zamrzla <josef.zamrzla@gmail.com>
2022-04-26 14:22:42 +0200

    esm fix

--- pgtw.js
+++ pgtw.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
-import { Pool, types } from 'pg';
+import * as postgres from 'pg';
 import camelcaseKeys from 'camelcase-keys';
 import microtime from 'microtime';
 import dbg from 'debug';
+
+const { Pool, types } = postgres.default;
 
 types.setTypeParser(types.builtins.INT8, value => parseInt(value, 10));
 
